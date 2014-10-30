66a8a29754b74ced4e4c96edec277fa43406fd65
Tim Stadler <coczero@users.noreply.github.com>
2014-10-30 19:26:37 +0000

    Added a lot of comments Small changes Added ability to water your fields

--- ConsoleFarmingSimulator/Shop.cs
+++ ConsoleFarmingSimulator/Shop.cs
@@ -3,27 +3,38 @@ using System.Collections.Generic;
 
 namespace ConsoleFarmingSimulator
 {
+  /// <summary>
+  /// Shop class to buy and sell items
+  /// </summary>
   class Shop
   {
     private Dictionary<Seed, double> _soldSeeds;
 
+    /// <summary>
+    /// Dictionary with seeds as keys and prices for values
+    /// </summary>
     public Dictionary<Seed, double> SoldSeeds
     {
       get { return _soldSeeds; }
       private set { _soldSeeds = value; }
     }
 
+    /// <summary>
+    /// Initializes a new shop
+    /// </summary>
     public Shop()
     {
       _soldSeeds = new Dictionary<Seed, double>();
       _soldSeeds.Add(Standards.Seeds.GetStandardSeed("Cucumber"), 0.5);  
     }
 
+    /// <summary>
+    /// Prints information about all items that are for sale
+    /// </summary>
     public void ShowSoldItems()
     {
       Console.WriteLine("Seeds: ");
       Console.WriteLine();
-
 
       int i = 1;
       foreach (KeyValuePair<Seed, double> entry in SoldSeeds)
@@ -36,6 +47,10 @@ namespace ConsoleFarmingSimulator
       }
     }
 
+    /// <summary>
+    /// Buys an item 
+    /// </summary>
+    /// <param name="name">Name of the item to buy</param>
     public void BuyItem(string name)
     {
       foreach (KeyValuePair<Seed, double> entry in SoldSeeds)
@@ -48,7 +63,7 @@ namespace ConsoleFarmingSimulator
             Program.Game.AddSeedToInventory(entry.Key);
           }
           else
-            throw new Exception("Not enough money!");
+            throw new Exception("Not enough money!"); //TODO: exception -> Game.Money
         }
       }
     }
